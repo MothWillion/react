6712919949de68ecae6299bd5f9bdb05d645da20
sunjun <sunjun@conzhu.com>
2019-12-13 09:54:22 +0800

    react-ssr store 实现

--- react-ssr/lesson02/client/index.js
+++ react-ssr/lesson02/client/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from '../src/App'
+import store from '../src/store/store'
+import { Provider } from 'react-redux'
 
-const Page = <BrowserRouter>
-    <App></App>
-</BrowserRouter>
+const Page = <Provider store={store}>
+    <BrowserRouter>
+        <App></App>
+    </BrowserRouter>
+</Provider>
 // 注水
 ReactDom.hydrate(Page, document.getElementById('root'))
--- react-ssr/lesson02/server/index.js
+++ react-ssr/lesson02/server/index.js
@@ -3,15 +3,19 @@ import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import express from 'express'
 import App from '../src/App'
+import store from '../src/store/store'
+import { Provider } from 'react-redux'
 
 const app = express()
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
     // 把react组件解析成html
-    const Page = <StaticRouter location={req.url}>
-        <App></App>
-    </StaticRouter>
+    const Page = <Provider store={store}>
+        <StaticRouter location={req.url}>
+            <App></App>
+        </StaticRouter>
+    </Provider>
     const content = renderToString(Page)
     // 字符串模板
     res.send(`
